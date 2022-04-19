c6b5e4823ed10cca24c7f251efe91535640bef53
lelaranja <leticiajlaranja@gmail.com>
2022-04-19 15:08:36 -0300

    Personagem 2 e Personagem 3 finalizados

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* Transições páginas Elsa*/
+
 function irPag2Elsa() {
     var escolha = prompt('Qual a sua escolha?')
     var tentativa = 1
@@ -48,12 +50,14 @@ function irPagFinalElsa() {
         }
         tentativa--;
     }
-    if (escolha.toLowerCase() == "a") {
-        return location = './pagFinalElsa.html'
+    if (escolha.toLowerCase() == "b") {
+        return location = './pagVitoriaElsa.html'
     } else {
         return location = './gameover3Elsa.html'
     }
 }
+
+/* Transições páginas Anna*/
 function irPag2Anna() {
     var escolha = prompt('Qual a sua escolha?')
     var tentativa = 1
@@ -103,8 +107,66 @@ function irPagFinalAnna() {
         tentativa--;
     }
     if (escolha.toLowerCase() == "a") {
-        return location = './pagAnnaFinal.html'
+        return location = './pagVitoriaAnna.html'
     } else {
         return location = './gameoverpag3Anna.html'
+    }
+}
+
+/* Transições páginas Kristoff*/
+function irPag2Kristoff() {
+    var escolha = prompt('Qual a sua escolha?')
+    var tentativa = 1
+    while (escolha.toLowerCase() != "a" && escolha.toLowerCase() != "b") {
+        if (tentativa == 0) {
+            return location = './gameoverKristoff.html'
+        } else {
+            alert(`Resposta inválida! Você tem mais ${tentativa} tentativa`)
+            escolha = prompt('Qual a sua escolha?')
+        }
+        tentativa--;
+    }
+    if (escolha.toLowerCase() == "b") {
+        return location = './pagKristoff2.html'
+    } else {
+        return location = './gameoverpag1Kris.html'
+    }
+}
+
+function irPag3Kristoff() {
+    var escolha = prompt('Qual a sua escolha?')
+    var tentativa = 1
+    while (escolha.toLowerCase() != "a" && escolha.toLowerCase() != "b") {
+        if (tentativa == 0) {
+            return location = './gameoverKristoff.html'
+        } else {
+            alert(`Resposta inválida! Você tem mais ${tentativa} tentativa`)
+            escolha = prompt('Qual a sua escolha?')
+        }
+        tentativa--;
+    }
+    if (escolha.toLowerCase() == "b") {
+        return location = './pagKristoff3.html'
+    } else {
+        return location = './gameoverpag2Kris.html'
+    }
+}
+
+function irPagFinalKristoff() {
+    var escolha = prompt('Qual a sua escolha?')
+    var tentativa = 1
+    while (escolha.toLowerCase() != "a" && escolha.toLowerCase() != "b") {
+        if (tentativa == 0) {
+            return location = './gameoverKristoff.html'
+        } else {
+            alert(`Resposta inválida! Você tem mais ${tentativa} tentativa`)
+            escolha = prompt('Qual a sua escolha?')
+        }
+        tentativa--;
+    }
+    if (escolha.toLowerCase() == "b") {
+        return location = './pagVitoriaKris.html'
+    } else {
+        return location = './gameoverpag3Kris.html'
     }
 }
